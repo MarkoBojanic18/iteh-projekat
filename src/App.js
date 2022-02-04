e130cdadaa11f0591503a5eddeca2cec99644539
MarkoBojanic18 <markobojanic18@gmail.com>
2022-02-04 01:37:10 +0100

    Odradjen je kompletan login, logout i register deo. Sve lepo funkcionise, istestirano vise puta!

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
+import {BrowserRouter as Router,Redirect,Route,Switch} from 'react-router-dom';
 
  import MasterLayout from './layouts/admin/MasterLayout';
 import Home from './components/frontend/Home';
@@ -13,6 +13,11 @@ axios.defaults.withCredentials = true;
 axios.defaults.baseURL = "http://localhost:8000";
 axios.defaults.headers.post['Content-Type']='application/json';
 axios.defaults.headers.post['Accept'] = 'application/json';
+axios.interceptors.request.use(function (config){
+  const token = localStorage.getItem('auth_token');
+  config.headers.Authorization = token ? `Bearer ${token}`:'';
+  return config;
+});
 
 function App() {
   return (
@@ -21,10 +26,16 @@ function App() {
         <Switch>
 
           <Route  exact path='/' component={Home}/>
-          <Route   path='/login' component={Login}/>
-          <Route   path='/register' component={Register}/>
+          {/* <Route   path='/login' component={Login}/>
+          <Route   path='/register' component={Register}/> */}
 
+          <Route path='/login'>
+            {localStorage.getItem('auth_token') ? <Redirect to='/' /> : <Login />}
+          </Route>
 
+          <Route path='/register'>
+            {localStorage.getItem('auth_token') ? <Redirect to='/' /> : <Register />}
+          </Route>
 
 
            <Route path='/admin' name="Admin" render={(props) => <MasterLayout {...props}/>} /> 
