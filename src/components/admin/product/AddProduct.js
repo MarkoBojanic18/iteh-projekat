ec37cbc7e6d3761b773c1d4fee98fefb5325daa6
Libmerwolf <danilo.gutesa99@gmail.com>
2022-02-09 12:34:08 +0100

    Kreiranje product page-a III

--- src/components/admin/product/AddProduct.js
+++ src/components/admin/product/AddProduct.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import axios from 'axios';
+import React, {useEffect} from 'react';
 import {Link} from 'react-router-dom';
 
 
 function AddProduct() {
+
+
+
+    useEffect(() => {
+      
+    axios.get(`/api/all-category`).then(res =>{
+    if (res.status === 200) {
+      console.log(res.data.category);
+      }
+});
+
+    }, []);
+    
+
 
     return (
         <div className="container-fluid px-4">
@@ -14,31 +29,103 @@ function AddProduct() {
             </div>
             <div className='card-body'>
 
-                <ul class="nav nav-tabs" id="myTab" role="tablist">
-                    <li class="nav-item" role="presentation">
-                        <button class="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home" type="button" role="tab" aria-controls="home" aria-selected="true">Home</button>
+                <form encType='multipart/form-data'>
+
+                <ul className="nav nav-tabs" id="myTab" role="tablist">
+                    <li className="nav-item" role="presentation">
+                        <button className="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home" type="button" role="tab" aria-controls="home" aria-selected="true">Home</button>
                     </li>
-                    <li class="nav-item" role="presentation">
-                        <button class="nav-link" id="seotags-tab" data-bs-toggle="tab" data-bs-target="#seotags" type="button" role="tab" aria-controls="seotags" aria-selected="false">Profile</button>
+                    <li className="nav-item" role="presentation">
+                        <button className="nav-link" id="seotags-tab" data-bs-toggle="tab" data-bs-target="#seotags" type="button" role="tab" aria-controls="seotags" aria-selected="false">SEO Tags</button>
                     </li>
-                    <li class="nav-item" role="presentation">
-                        <button class="nav-link" id="otherdetails-tab" data-bs-toggle="tab" data-bs-target="#otherdetails" type="button" role="tab" aria-controls="otherdetails" aria-selected="false">Contact</button>
+                    <li className="nav-item" role="presentation">
+                        <button className="nav-link" id="otherdetails-tab" data-bs-toggle="tab" data-bs-target="#otherdetails" type="button" role="tab" aria-controls="otherdetails" aria-selected="false">Other Details</button>
                     </li>
                 </ul>
-                <div class="tab-content" id="myTabContent">
-                    <div class="tab-pane card-body border fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
-                        a
+                 <div className="tab-content" id="myTabContent">
+                    <div className="tab-pane card-body border fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+                        
+
+                    <div className='form-group mb-3'>
+                        <label>Select Category</label>
+                        <select name="categoty_id" className='form-control'>
+                            <option>Select Category</option>
+                        </select>
                     </div>
-                    <div class="tab-pane card-body border fade" id="seotags" role="tabpanel" aria-labelledby="seotags-tab">
-                        b
+
+                    <div className='form-group mb-3'>
+                        <label>Slug</label>
+                        <input type="text" name = "slug" className='form-control'/>
                     </div>
-                    <div class="tab-pane card-body border  fade" id="otherdetails" role="tabpanel" aria-labelledby="otherdetails-tab">
-                        c
+                    <div className='form-group mb-3'>
+                        <label>Name</label>
+                        <input type="text" name = "name" className='form-control'/>
+                    </div>
+                    <div className='form-group mb-3'>
+                        <label>Description</label>
+                        <textarea name ="description" className='form-control'></textarea>
+                    </div>
+                    
+                    </div>
+                    <div className="tab-pane card-body border fade" id="seotags" role="tabpanel" aria-labelledby="seotags-tab">
+                        
+                        <div className='form-group mb-3'>
+                            <label>Meta Title</label>
+                            <input type="text" name = "meta_title" className='form-control'/>
+                        </div>
+                       <div className='form-group mb-3'>
+                        <label>Meta Keyword</label>
+                        <textarea name ="meta_keyword" className='form-control'></textarea>
+                        </div>
+                         <div className='form-group mb-3'>
+                        <label>Meta Description</label>
+                        <textarea name ="meta_descrip" className='form-control'></textarea>
+                         </div>
+                    </div>
+                    <div className="tab-pane card-body border  fade" id="otherdetails" role="tabpanel" aria-labelledby="otherdetails-tab">
+                        
+                        <div className='row'>
+                            <div className='col-md-4 form-group mb-3'>
+                                <label>Selling Price</label>
+                                <input type="text" name = "selling_price" className='form-control'/>
+                            </div>
+                             <div className='col-md-4 form-group mb-3'>
+                                <label>Original Price</label>
+                                <input type="text" name = "original_price" className='form-control'/>
+                            </div>
+                             <div className='col-md-4 form-group mb-3'>
+                                <label>Quantity</label>
+                                <input type="text" name = "qty" className='form-control'/>
+                            </div>
+                             <div className='col-md-4 form-group mb-3'>
+                                <label>Brand</label>
+                                <input type="text" name = "brand" className='form-control'/>
+                            </div>
+                             <div className='col-md-8 form-group mb-3'>
+                                <label>Image</label>
+                                <input type="file" name = "image" className='form-control'/>
+                            </div>
+                             <div className='col-md-4 form-group mb-3'>
+                                <label>Featured (checked=shown) </label>
+                                <input type="checkbox" name = "featured" className='w-50 h-50' />
+                            </div>
+                             <div className='col-md-4 form-group mb-3'>
+                                <label>Popular (checked=shown) </label>
+                                <input type="checkbox" name = "popular" className='w-50 h-50' />
+                            </div>
+                            <div className='col-md-4 form-group mb-3'>
+                                <label>Status (checked=Hidden) </label>
+                                <input type="checkbox" name = "status" className='w-50 h-50' />
+                            </div>
+
+
+                        </div>
+
                     </div>
                 </div>
-
-
-            </div>
+                <button type ="submit" className='btn btn-primary px-4 mt-2'>Submit</button>
+                </form>
+             </div>
          </div>
         </div>
     )
